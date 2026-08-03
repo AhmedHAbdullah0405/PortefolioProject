964cd58440fac362814a26af498823c2b7214374
AhmedHAbdullah0405 <ahbe001@student.kristiania.no>
2026-08-03 22:26:53 +0200

    Added all my letters oif reccomendation inside a "letters" folder. Created a "downloadPDF" method that downloads one of the letters and puts it iside the users "Downloads" folder on their computer

--- portefoliopage/src/pages/WorkExperience.tsx
+++ portefoliopage/src/pages/WorkExperience.tsx
@@ -1,16 +1,34 @@
-function workExperience(){
+function workExperience() {
 
-    return(
+    const pdfContainer = "portefoliopage\public\letters\Ahmed Abdullah_StudentAssistent_Sluttattest.pdf"
+
+    const downloadPDF = () => {
+        const link = document.createElement("a");
+        link.href = pdfContainer;
+
+        link.setAttribute("download", pdfContainer)
+
+        document.body.appendChild(link)
+
+        link.click();
+
+        document.body.removeChild(link);
+    }
+
+
+
+    return (
         <>
-            <h2>Tidligere erfaringer</h2> 
+            <h2>Tidligere erfaringer</h2>
             <h3>Postbud</h3> Posten Røa Distribusjon &ensp; 29.09/25 - d.d <br />
             Distribusjon av pakker
             for posten Røa
-        
+
             <br />
             <h3>Studentassistent</h3> Høyskolen Kristiania &ensp; 20.01/25 - 30.06/25 <br />
             Veileder for studenter i
             emnet PG2202 - Unity Utvikling
+            <button className="download" onClick={downloadPDF}>Click here to download this jobs referal</button>
 
             <br />
             <h3>Distributør</h3> Helthjem &ensp; 19.06/23 - 25.09/23 <br />
@@ -22,11 +40,11 @@ function workExperience(){
             <h3>Telefonselger</h3> Decision AS &ensp; 20.01/22 - 13.01/23 <br />
             Salg av strøm for
             Agva Kraft via telefonen
-        
-        
-        
-        
-        
+
+
+
+
+
         </>
     )
 
