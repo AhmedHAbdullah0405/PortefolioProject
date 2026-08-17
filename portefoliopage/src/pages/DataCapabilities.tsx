730c2d2755148570822dfa9f61d4ec19028916ca
AhmedHAbdullah0405 <ahbe001@student.kristiania.no>
2026-08-17 21:27:33 +0200

    La til bilder for alle backedn, frontend og sikkerhetesverktøy. Modifisert css for å style bildene og fjenret vekk teksten for e trhe kategoriene

--- portefoliopage/src/pages/DataCapabilities.tsx
+++ portefoliopage/src/pages/DataCapabilities.tsx
@@ -5,21 +5,38 @@ function dataCapabilities(){
             <h2>Datakunnskap</h2>
             
             <div id="backend">
-                <h3 className="backendTitle">Backendverktøy:</h3>
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" /> 
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" />
+                <h3 id="backendTitle">Backendverktøy:</h3>
+                <div className="backend_img">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" alt="mysqlWorkbenchIcon" width={150}/> 
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" alt="javaIcon" width={150}/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" alt="pythonIcon" width={150}/>
+                </div>    
             </div>
             
+            <br />
            
-            <h3>Sikkerhetsverktøy:</h3>
-            <div>
-                VMware Workstation Pro / VMware Workstation Player 17, OWASP ZAP,
-                Splunk, Wireshark, Volatility3, Yara.
+            <div id="security">
+                <h3 id="securityTitle">Sikkerhetsverktøy:</h3>
+                <div className="security_img">
+                    <img src="./Vmware_workstation_16_icon.svg" alt="vmwareWorkstation16Icon" width={150} />
+                    <img src="./zap.ico" alt="owaspZapIcon" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/splunk/splunk-original-wordmark.svg" alt="splunkIcon" width={150}/>
+                    <img src="./Wireshark_icon.svg" alt="wiresharkIcon" width={150} />
+                    <img src="./volatility3.png" alt="volatiltyIcon" />
+                    <img src="./Yara_logo.svg" alt="yaraIcon" width={150} />
+                </div>
             </div>
 
-            <h3>Frontendverktøy:</h3>
-            <div>JavaScript, TypeScript, React.</div>
+            <br />
+            
+            <div id="frontend">
+                <h3 id="frontEndTitle">Frontendverktøy:</h3>
+                <div className="frontend_img">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" width={150}/>
+                </div>
+            </div>
          
             <h3>Spillutvikling verktøy:</h3>
             <div>C#, Unity</div>
