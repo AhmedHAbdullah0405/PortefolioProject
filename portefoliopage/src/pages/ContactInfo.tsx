85811aab2e74027014f87adfa84c3aeb7e813c90
AhmedHAbdullah0405 <ahbe001@student.kristiania.no>
2026-08-20 09:36:19 +0200

    Added images, fixed css and madofied som proeprteis for said images in the .tsx file

--- portefoliopage/src/pages/ContactInfo.tsx
+++ portefoliopage/src/pages/ContactInfo.tsx
@@ -4,7 +4,7 @@ function contactInfo(){
         <>
             <h1>Contact Info</h1>
             <div className="card">
-                <img src="../images/Oppdatert bilde av Ahmed.jpg" alt="profilePicture" width={150} />
+                <img src="./images/Oppdatert bilde av Ahmed.jpg" alt="profilePicture" width={150} />
                 <div>Mobil: 95428728</div>
                 <div>Epost: ahmed030503@gmail.com</div>
                 <div>Linkedin: www.linkedin.com/in/ahmed-abdullah-a2b4b0278</div>
--- portefoliopage/src/pages/DataCapabilities.tsx
+++ portefoliopage/src/pages/DataCapabilities.tsx
@@ -19,36 +19,66 @@ function dataCapabilities(){
                 <h3 id="securityTitle">Sikkerhetsverktøy:</h3>
                 <div className="security_img">
                     <img src="./Vmware_workstation_16_icon.svg" alt="vmwareWorkstation16Icon" width={150} />
-                    <img src="./zap.ico" alt="owaspZapIcon" width={150} />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/splunk/splunk-original-wordmark.svg" alt="splunkIcon" width={150}/>
-                    <img src="./Wireshark_icon.svg" alt="wiresharkIcon" width={150} />
-                    <img src="./volatility3.png" alt="volatiltyIcon" />
                     <img src="./Yara_logo.svg" alt="yaraIcon" width={150} />
+                    <img src="./Wireshark_icon.svg" alt="wiresharkIcon" width={150}/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/splunk/splunk-original-wordmark.svg" alt="splunkIcon" width={175}/>
+                    <img src="./zap.ico" alt="owaspZapIcon" width={150} />            
+                    <img src="./volatility3.png" alt="volatiltyIcon" width={350}/>
+                    
                 </div>
             </div>
 
             <br />
             
             <div id="frontend">
-                <h3 id="frontEndTitle">Frontendverktøy:</h3>
+                <h3 id="frontendTitle">Frontendverktøy:</h3>
                 <div className="frontend_img">
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" width={150} />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" width={150} />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" width={150}/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="javascriptIcon" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" alt="typeScriptIcon" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt="reactIcon" width={150}/>
                 </div>
             </div>
          
-            <h3>Spillutvikling verktøy:</h3>
-            <div>C#, Unity</div>
+            <br />
+
+            <div id="gamedev">
+                <h3 id="gameDevTitle">Spillutvikling verktøy:</h3>
+                <div className="gamedev_img">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-original.svg" alt="unityIcon" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" alt="C#Icon" width={150} />
+                </div>
+            </div>
             
-            <h3>IoT verktøy:</h3>
-            <div>Arduino IDE</div>
+            <br />
             
-            <h3>UI verktøy:</h3>
-            <div>Shadcn UI, Tableau, HTML og CSS</div>
+            <div id="IoT">
+                <h3 id="IoTTitle">IoT verktøy:</h3>
+                <div className="IoT_img">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original-wordmark.svg" alt="ArduinoIcon" width={150} />
+                </div>
+            </div>
+
+            <br />
             
-            <h3>KI:</h3>
-            <div>ChatGPT og Claude for effektivisering av kode og debugging</div>
+            <div id="UI">
+                <h3 id="UITitle">UI verktøy:</h3>
+                <div className="UI_img">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" alt="html5Icon" width={150} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" alt="css3Icon" width={150} />
+                    <img src="./shadcnlogo.png" alt="shadcnIcon" width={125} />
+                    <img src="./tableaulogo.png" alt="tableauIcon" width={250} />
+                    
+                </div>
+            </div>
+
+            <br />
+            
+            <div id="AI">
+                <h3>KI:</h3>
+                <div>ChatGPT og Claude for effektivisering av kode og debugging</div>
+
+            </div>
+           
         </>
     )
 
